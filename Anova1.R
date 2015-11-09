266d70f621f70ff235e8ac4c70feab403aa80223
k3xr <k3ther@gmail.com>
2015-11-10 00:14:40 +0100

    anova script fixes

--- Anova1.R
+++ Anova1.R
@@ -7,35 +7,39 @@ library(multcomp)
 ## Multicollinearity
 # we use a data set known for its high multicollinearity
 data(longley)
+
 # we create a subset with the regressor variables only
 long=as.matrix(longley[,-7])
 
 # function r2multv
 r2multv<-function(x){
-r2s=1-1/(diag(solve(cov(x)))*diag(cov(x)))
-r2s
+  r2s=1-1/(diag(solve(cov(x)))*diag(cov(x)))
+  r2s
 }
 
 # function myvif
 myvif<-function(x){
- myvif=1/(1-r2multv(x))
- myvif
-sort(myvif, decreasing=TRUE)
-  }
+  myvif=1/(1-r2multv(x))
+  myvif
+  sort(myvif, decreasing=TRUE)
+}
 
 # we use it, remember vif's > 10 indicate high multicollinearity
- myvif(long)
+myvif(long)
 
 ## visualizing collinearity directly, package FactoMineR
 
 ## which variables have strong linear correlations?
 long_pca=PCA(long,scale.unit=TRUE, ncp=6, axes=c(1,2))
+
 # eigenvalues, notice the almost 0 value of the last 3 ones.
 long_pca$eig
+
 # eigenvectors associated
 vectors=data.frame(long_pca$svd$V)
 rownames(vectors)=colnames(long)
 vectors
+
 ## which variables contribute the most to last dimensions? We can see nothing, we are trying to project 3-dimensional data set on
 # a fifth and sixth dimensions
 plot(long_pca, shadow=TRUE,choix="var", select="contrib 4" , axes=c(5,6))
@@ -46,8 +50,11 @@ barplot(long_pca$var$contrib, beside=TRUE, legend.text=TRUE,args.legend=list(x="
 princomp(long, cor=TRUE)$loadings
 
 ## OPTIONAL: Another way: Multicollinearity diagnostic with package perturb.
-# The variance decomposition proportions associated with each condition index. If a large condition index is associated two or more variables #with large variance decomposition proportions, these variables may be causing collinearity problems. Belsley et al suggest that a large #proportion is 50 percent or more.
+# The variance decomposition proportions associated with each condition index. If a large condition index is associated
+# two or more variables #with large variance decomposition proportions, these variables may be causing collinearity problems.
+# Belsley et al suggest that a large proportion is 50 percent or more.
 colldiag(long, center=T)
+
 ### OPTIONAL finishes ##
 
 ## Principal Component Regression
@@ -55,16 +62,19 @@ colldiag(long, center=T)
 ## variable selection procedures
 ## we create a set for the running variables in the model
 run=hbat[,6:19]
+
 # We are using function step, by default it compares models based in AIC. We have to define Base model and Full model.
 Base1=lm(X19~1, data=run)
 Full1=lm(X19~., data=run)
 Step.for=step(Base1, scope=list(upper=Full1, lower=~1),direction="forward",trace=F)
+
 # if we remove trace=F we get the sequential process printed out in our screen
 summary(Step.for)
 Step.for$anova
 
 ## backward procedure
 Step.back=step(Full1,direction="backward",trace=F)
+
 ## direction both
 Step.both=step(Base1, scope=list(upper=Full1, lower=~1),direction="both",trace=F)
 
@@ -79,9 +89,11 @@ summary(Step2.for)
 # package leaps to use Mallow's Cp, it does an exhaustive search
 Cp=leaps(x=hbat[,6:18],y=hbat[,19],names=names(hbat)[6:18],method="Cp")
 plot(Cp$size,Cp$Cp, ylim=c(2,14), xlim=c(2,14))
+
 # Adding the line Cp=p (x=y) 
 abline(0,1)
 identify(Cp$size,Cp$Cp)
+
 # We identify the model in row 42, which has variables
 Cp$which[42,]
 
@@ -92,6 +104,7 @@ cbind(Cp$size[c(42,117,121)], Cp$Cp[c(42,117,121)])
 
 # package leaps using adjr2 as the criiterion
 Adjr2=leaps(x=hbat[,6:18],y=hbat[,19],names=names(hbat)[6:18],method="adjr2")
+
 # the model with maximum adjr2 is
 Adjr2$which[which(Adjr2$adjr2==max(Adjr2$adjr2)),] 
 
@@ -102,6 +115,7 @@ plot(subsets,scale="bic")
 plot(subsets,scale="Cp")
 summary(subsets)
 coef(subsets,5)
+
 #### Variable selection procedures finish
 
 #Transformations
@@ -109,7 +123,8 @@ powerTransform(hbat[,6:18])
 powerTransform(lm.1)
 
 ## OPTIONAL
-## restricting our attention to that variables in lm.1, looking for the best model including two order interactions between the variables X's
+# restricting our attention to that variables in lm.1, looking for the best model including two order 
+# interactions between the variables X's
 dependent=hbat[,c(6,7,9,11,12,19)]
 Base=lm(X19~1, data=dependent)
 FullB=lm(X19~.^2, data=dependent)
@@ -119,10 +134,12 @@ summary(StepB.for)
 gvlma(StepB.for)
 
 X=hbat[,c(9,6,12,7,11)]
+
 # We have important problems with multicollinearity
 myvif(X)
 Xs=data.frame(scale(X,scale=F))
 myvif(Xs)
+
 # multicollinearity problems dissapear, the model is the same
 summary(lm(dependent$X19~Xs$X9+Xs$X6+Xs$X12+Xs$X7+Xs$X11+Xs$X12:Xs$X11+Xs$X6:Xs$X12+Xs$X7:Xs$X12))
 plot(lm(dependent$X19~Xs$X9+Xs$X6+Xs$X12+Xs$X7+Xs$X11+Xs$X12:Xs$X11+Xs$X6:Xs$X12+Xs$X7:Xs$X12))
